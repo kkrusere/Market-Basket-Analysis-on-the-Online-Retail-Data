06eba8ca4c7580ea339800198be7724c2a888bc2
krusere04 <krusere@saintpeters.edu>
2022-07-19 13:23:03 -0400

    update

---
+++ MBA_App.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+
+st.set_page_config( page_title="Market Basket Analysis App",
+                    page_icon= "random",
+                    layout="wide"
+ )
+
+
+
+col1, col2, col3 = st.columns((.1,1,.1))
+
+with col1:
+    st.write("")
+
+with col2:
+    st.markdown(" <h1 style='text-align: center;'>Market Basket Analysis on Online Retail Data:</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'><i><b>Providing a Retail Business with a strategy which helps improve their "
+                "product sales, inventory management, and customer retention, intern improving the profitability of the business.</b></i></p>", unsafe_allow_html=True)
+    st.markdown(" <center><img src='Assets/MBA.jpeg' width=600/></center>", unsafe_allow_html=True)
+    
+
+    
+
+with col3:
+    st.write("")
+
+st.markdown("### ***Project Contributors:***")
+st.markdown("Kuzi Rusere")
+
