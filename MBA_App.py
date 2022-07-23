a0d2e9f52681c745e971245b5e39f79f4d14ecef
krusere04 <krusere@saintpeters.edu>
2022-07-22 20:48:54 -0400

    update

--- MBA_App.py
+++ MBA_App.py
@@ -90,37 +90,27 @@ with col1:
                 "tool that allows users interact with the data and analytics.")
 
 with col2:
-    st.markdown("### **Data Collection**")
+    st.markdown("### ***Data Collection***")
 
     """
+    **General Information About the Data**
 
-    **General Information About the Data** <br>
-    This is a transnational data set which contains all the transactions occurring between 01/12/2010 and 09/12/2011 for a UK-based and registered non-store online retail. The company mainly sells unique all-occasion gifts. Many customers of the company are wholesalers. <br>
+    This is a transnational data set which contains all the transactions occurring between 01/12/2010 and 09/12/2011 for a UK-based and registered non-store online retail. The company mainly sells unique all-occasion gifts. Many customers of the company are wholesalers.
 
-    **Information about the Attributes/Columns in the Dataset** <br>
+    **Information about the Attributes/Columns in the Dataset**
+    * ***InvoiceNo:*** Invoice number. Nominal, a 6-digit integral number uniquely assigned to each transaction. If this code starts with letter 'c', it indicates a cancellation.
+    * ***StockCode:*** Product (item) code. Nominal, a 5-digit integral number uniquely assigned to each distinct product.
+    * ***Description:*** Product (item) name. Nominal.
+    * ***Quantity:*** The quantities of each product (item) per transaction. Numeric.
+    * ***InvoiceDate:*** Invice Date and time. Numeric, the day and time when each transaction was generated.
+    * ***UnitPrice:*** Unit price. Numeric, Product price per unit in sterling.
+    * ***CustomerID:*** Customer number. Nominal, a 5-digit integral number uniquely assigned to each customer.
+    * ***Country:*** Country name. Nominal, the name of the country where each customer resides.
 
-    ***InvoiceNo:*** Invoice number. Nominal, a 6-digit integral number uniquely assigned to each transaction. If this code starts with letter 'c', it indicates a cancellation. <br>
-
-    ***StockCode:*** Product (item) code. Nominal, a 5-digit integral number uniquely assigned to each distinct product. <br>
-
-    ***Description:*** Product (item) name. Nominal. <br>
-
-    ***Quantity:*** The quantities of each product (item) per transaction. Numeric.<br>
-
-    ***InvoiceDate:*** Invice Date and time. Numeric, the day and time when each transaction was generated. <br>
-
-    ***UnitPrice:*** Unit price. Numeric, Product price per unit in sterling.<br>
-
-    ***CustomerID:*** Customer number. Nominal, a 5-digit integral number uniquely assigned to each customer. <br>
-
-    ***Country:*** Country name. Nominal, the name of the country where each customer resides. <br>
-
-
+    The data source:
+    
     """
-
-
-
-
+    st.image("", caption="https://archive.ics.uci.edu/ml/datasets/online+retail")
 
 
 
