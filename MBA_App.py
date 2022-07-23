f0b6a68b4cb55c12b23f3c80b139dfff1fccee43
krusere04 <krusere@saintpeters.edu>
2022-07-23 19:08:18 -0400

    update

--- MBA_App.py
+++ MBA_App.py
@@ -746,13 +746,15 @@ st.dataframe(basket.head())
 st.markdown("The next step will be to generate the frequent itemsets that have a support of at least 10% using the MLxtend Apriori fuction which returns frequent itemsets from a one-hot DataFrame.")
 
 frequent_itemsets = apriori(basket, min_support=0.01, use_colnames=True)
+frequent_itemsets.to_csv("frequent_itemsets.csv", index=False)
+frequent_itemsets = pd.read_csv("frequent_itemsets.csv")
 
 col1, col2, col3= st.columns((.1,1,.1))
 with col1:
     pass
 with col2:
     """Frequent Itemsets"""
-    #st.dataframe(frequent_itemsets.head())
+    st.dataframe(frequent_itemsets.head())
 with col3:
     pass
 
